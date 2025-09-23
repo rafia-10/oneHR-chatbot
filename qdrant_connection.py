5dccf16ae7e3d46b8496fe1a5fd7a61d6ee2634d
Rafia Kedir <rafiakedir22@gmail.com>
2025-09-23 10:12:03 +0000

    Edit qdrant_connection.py

--- qdrant_connection.py
+++ qdrant_connection.py
@@ -1,10 +1,17 @@
-from google.colab import userdata
+import os 
 from qdrant_client import QdrantClient
 
-api_key = userdata.get('QDRANT_API_KEY')
-url= userdata.get('QDRANT_URL')
+api_key = os.environ.get('QDRANT_API_KEY')
+url= os.environ.get('QDRANT_URL')
 # 🔑 Connect to Qdrant
 qdrant_client = QdrantClient(
     url= url, 
     api_key= api_key,
+
+
 )
+try:
+    qdrant_client.get_collections()
+    print("Successfully connected to Qdrant!")
+except Exception as e:
+    print(f"Failed to connect to Qdrant: {e}")
