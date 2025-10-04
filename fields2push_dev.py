c1544f27f2f94230b4331d81acaf962ba341972e
rafia-10 <rafiaa05k@gmail.com>
2025-10-04 14:47:52 +0300

    selected fields to push to qdrant from both databases

---
+++ fields2push_dev.py
@@ -0,0 +1,51 @@
+fields_to_push_dev = {
+    "employee": [
+        "role",
+        "employmentPosition",
+        "reportingLineManagerPosition",
+        "shiftType"
+    ],
+    "attendance": [
+        "comments"
+    ],
+    "objective": [
+        "employeeFeedback",
+        "managerFeedback",
+        "justification",
+        "actionItems",
+        "managerMessage"
+    ],
+    "overtimeRequest": [
+        "overtimeGoal",
+        "overtimeJustification",
+        "hrComments"
+    ],
+    "performanceEvaluation": [
+        "comment"
+    ],
+    "leaveManagement": [
+        "reason",
+        "comments"
+    ],
+    "jobPost": [
+        "jobDescription",
+        "responsibilities",
+        "requirements",
+        "benefits"
+    ],
+    "hiringNeed": [
+        "reasonForHire",
+        "rejectionReason"
+    ],
+    "trainingMaterial": [
+        "trainingOutcome",
+        "trainingJustification",
+        "employeeFeedbacks",
+        "comments",
+        "sections"
+    ],
+    "talentPool": [
+        "levelOfEducation",
+        "cvDocument"
+    ]
+}
