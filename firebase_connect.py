283baaa3f2711edf62e2e0699f4fc666916eb2fa
rafia-10 <rafiaa05k@gmail.com>
2025-10-10 09:26:59 +0300

    initilaise dev app

--- firebase_connect.py
+++ firebase_connect.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials, firestore
 # OneHR Dev
 cred_dev = credentials.Certificate("secrets/onehr-dev.json")
 app_dev =firebase_admin.initialize_app(cred_dev, name="dev")
-db_dev = firestore.client()
+db_dev = firestore.client(app=app_dev)
 
 # OneHR Internal
 cred_internal = credentials.Certificate("secrets/onehr-internal.json")
