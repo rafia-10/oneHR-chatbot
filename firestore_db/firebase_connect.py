b8465a4ec920a4a2ea417ccf798200b731dd56aa
rafia-10 <rafiaa05k@gmail.com>
2025-10-22 12:20:19 +0300

    add firebase connection logic

---
+++ firestore_db/firebase_connect.py
@@ -0,0 +1,14 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# OneHR Dev
+cred_dev = credentials.Certificate("secrets/onehr-dev.json")
+app_dev =firebase_admin.initialize_app(cred_dev, name="dev")
+db_dev = firestore.client(app=app_dev)
+
+# OneHR Internal
+cred_internal = credentials.Certificate("secrets/onehr-internal.json")
+app_internal = firebase_admin.initialize_app(cred_internal, name="internal")
+db_internal = firestore.client(app=app_internal)
+
+db_clients = {"dev": db_dev, "internal": db_internal}
